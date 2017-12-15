dd86b93d8b8b809abdc74f0ab027616076042374
jangmijin <tlrud0621@gmail.com>
2017-12-15 12:27:58 +0900

    Express를 이용하여 간단한 웹앱 만들기  get 라우터를 사용하여 페이지 및 res.send를 통해 메시지 응답하는 법

---
+++ js/server_side_javascript/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+//http://localhost:3000/
+app.get('/',function(req, res){
+  res.send('hello home page');
+});
+//get 메소드를 라우터라고 부르고 get이하는 일을 라우팅이라고 부른다
+app.get('/login', function(req, res){
+  res.send('<h1>Login please</h1>');
+});
+app.get('/main', function(req, res){
+  res.send('<ul><li>Nodejs</li><li>express를 이용한 웹 어플리케이션 만들기</li></ul>')
+});
+app.get('/intro',function(req, res){
+  res.send('<h3>express 모듈을 이용해 get 메소드 즉 라우터를 사용하여 페이지를 만들고 응답 메시지도 보내봅니다.</h3>')
+})
+app.listen(3000, function(){
+  console.log('Conneted 3000 port!');
+});
